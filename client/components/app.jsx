069154313690e661231a2757e2535a9a9062f4cb
Mil Amirian <milamirian22@gmail.com>
2020-09-29 21:47:19 +0000

    completed Add To Cart Frontend

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -12,9 +12,13 @@ export default class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.getCartItems = this.getCartItems.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+    this.updateCartCountForUser = this.updateCartCountForUser.bind(this);
   }
 
   setView(name, params) {
@@ -26,27 +30,64 @@ export default class App extends React.Component {
     }));
   }
 
+  getCartItems() {
+    fetch('api/cart')
+      .then(res => res.json())
+      .then(cartItems => {
+        this.setState(state => ({
+          cart: this.state.cart.concat(cartItems)
+        }));
+      });
+  }
+
+  addToCart(product) {
+    const postOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        productId: product.productId
+      })
+    };
+    fetch('/api/cart', postOptions)
+      .then(res => res.json())
+      .then(cartItem => {
+        this.setState(state => ({
+          cart: this.state.cart.concat(cartItem[0])
+        }));
+      });
+  }
+
   componentDidMount() {
     fetch('/api/health-check')
       .then(res => res.json())
       .then(data => this.setState({ message: data.message || data.error }))
       .catch(err => this.setState({ message: err.message }))
       .finally(() => this.setState({ isLoading: false }));
+    this.getCartItems();
+  }
+
+  updateCartCountForUser() {
+    const cartCount = this.state.cart.length;
+    if (cartCount === 1) {
+      return `${cartCount} item`;
+    } else if (cartCount > 1) {
+      return `${cartCount} items`;
+    }
   }
 
   render() {
     if (this.state.view.name === 'catalog') {
       return (
         <div className="container-fluid">
-          <PageTitle text="Wicked Sales" />
+          <PageTitle text="Wicked Sales" cartItemCount={this.updateCartCountForUser()}/>
           <ProductList setView={this.setView}/>
         </div>
       );
     } else {
       return (
         <div className="container-fluid">
-          <PageTitle text="Wicked Sales" />
-          <ProductDetails id={this.state.view.params.productId} setView={this.setView}/>
+          <PageTitle text="Wicked Sales" cartItemCount={this.updateCartCountForUser()}/>
+          <ProductDetails id={this.state.view.params.productId} setView={this.setView} addToCart={this.addToCart}/>
         </div>
       );
     }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,7 +5,20 @@ function PageTitle(props) {
     <header>
       <div className="header row d-flex align-items-center">
         <div className="col-1"></div>
-        <h6 className="title col align-self-center pt-2"><b>$ </b>{props.text}</h6>
+        <div className="col-8">
+          <h6 className="title col align-self-center pt-2"><b>$ </b>{props.text}</h6>
+        </div>
+
+        <div className="col-2 d-flex align-items-center justify-content-center">
+          <div className="d-flex align-items-center">
+            <h6 className="cart-items mr-2">{props.cartItemCount}</h6>
+          </div>
+          <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" className="bi bi-cart4 ml-1" fill="whitesmoke" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+          </svg>
+        </div>
+        <div className="col-1"></div>
+
       </div>
     </header>
   );
